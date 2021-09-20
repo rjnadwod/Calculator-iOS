e1b7b92b864f5e7fa3da0cfca6288512053aae21
rjnadwod <riley.nadwodny@gmail.com>
2021-09-20 11:31:08 -0500

    Refactor view name

--- Shared/CalculatorView.swift
+++ Shared/CalculatorView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CalculatorView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct CalculatorView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        CalculatorView()
     }
 }
---
+++ Shared/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  calculator
+//
+//  Created by Riley Nadwodny on 9/20/21.
+//
+
+import Foundation
