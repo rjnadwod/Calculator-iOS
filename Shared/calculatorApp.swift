f373727efd3e8b276c0293b8a73bf3a46bb14a7d
rjnadwod <riley.nadwodny@gmail.com>
2021-09-20 11:28:08 -0500

    Initial Commit

---
+++ Shared/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  Shared
+//
+//  Created by Riley Nadwodny on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
